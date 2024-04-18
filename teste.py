37cc4932b3bacc5159d525afdfa0ccb1dd4da584
JP-Conde5 <jcgalves25@hotmail.com>
2024-04-18 17:18:38 -0300

    Retângulo desenhado

--- teste.py
+++ teste.py
@@ -1,9 +1,15 @@
 import pygame as py
+from pygame.locals import *
 
+#cores
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
 
 #inicializa o módulo, premitindo que seja interpretado
 py.init()
 
+#tela
 width = 640
 high = 480
 #determina o tamanho da tela
@@ -13,16 +19,45 @@ screen = py.display.set_mode((width, high))
 clock = py.time.Clock()
 running = True
 
+#desenho do sqr_1
+width_1 = 40
+x_1 = (width/2) - (width_1/2)
+y_1 = (high/2) - (width_1/2)
 while running:
+    #preenche a tela
+    screen.fill((255,255,255))
+    #atualiza toda a página
+    #py.display.flip()
+    #determina o número de frames
+    clock.tick(30)
     #percorre os eventos possíveis do pygame
     for event in py.event.get():
         #se o evento for o QUIT, para a loop
         if event.type == py.QUIT:
            running = False 
-    #preenche a tela
-    screen.fill((255,255,255))
-    #
-    py.display.flip()
-    #determina o número de frames
-    clock.tick(60)
+    #movimentação (.key dentro de pygame.locals)
+    if py.key.get_pressed()[K_a]:
+        x_1 = x_1 - 20
+    elif py.key.get_pressed()[K_d]:
+        x_1 = x_1 + 20
+    elif py.key.get_pressed()[K_w]:
+        y_1 = y_1 - 20
+    elif py.key.get_pressed()[K_s]:
+        y_1 = y_1 + 20
+    
+    #manter na tela
+    if x_1 >= width:
+        x_1 = 0
+    elif x_1 <= -width_1:
+        x_1 = width - width_1
+    elif y_1 >= width:
+        y_1 = 0
+    elif y_1 <= -width_1:
+        y_1 = width - width_1
+        
+    #objetos na tela    
+    sqr_1 = py.draw.rect(screen, red, (x_1, y_1, width_1, width_1))
+    py.display.update()
+
+    
 
